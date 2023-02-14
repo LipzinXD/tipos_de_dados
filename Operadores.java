daacfa8626dc449bcfa47acf0ab9be3bfd8d57cf
LipzinXD <felipescamargo806@gmail.com>
2023-02-14 14:39:44 +0000

    Salvando Operadores

--- Operadores.java
+++ Operadores.java
@@ -1,13 +1,22 @@
-public class Operadores {
-    public static void main(String[] Felipe){
-        
-        int idade;
-        idade = 16;
-        idade++;
-        idade++;
-        System.out.println("eu tenho "+ idade + " anos");
+import java.util.Scanner;
 
-        idade += 10;
+public class Operadores {
+    public static void main(String[] Felipe) {
+        Scanner entrada = new Scanner(System.in);
+        int idade=0;
+        int numero1;
+        int numero2;
+        int soma;
+        System.out.println ("Digite sua idade");
+        idade=entrada.nextInt();
         System.out.println(idade);
-    }
+      
+        System.out.println("Digite um número");
+        numero1=entrada.nextInt();
+
+        System.out.println("Digite um número");
+        numero2=entrada.nextInt();
+        soma=numero1+numero2;
+        System.out.println("A soma dos dois numeros é: " +soma);
+   }
 }
