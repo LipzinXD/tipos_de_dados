c6cb8423c4e82abcc38c326e076c3a8d017ab08a
LipzinXD <felipescamargo806@gmail.com>
2023-02-15 17:22:09 +0000

    Exercicios 15/02/23

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,3 +1,55 @@
 public class EstruturaDeRepeticao {
 
+    public static void main(String asrgs[]) {
+
+        // Exercicio 1
+
+        /*
+         * for (int i =150; i <=300; i++) {
+         * System.out.println(i);
+         * }
+         */
+
+        // Exercicio 2
+        
+        /*
+         * int soma=0;
+         * for (int i =1; i <= 1000; i++) {
+         * 
+         * soma=soma+i;
+         * 
+         * }
+         * System.out.println(soma);
+         */
+
+        // Exercicio 3
+
+        /*
+         * for (int i =3; i <= 100; i+=3) {
+         * System.out.println(i);
+         * }
+         */
+
+        // Exercicio 4
+
+        /*int fatorial = 1;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+
+        }*/
+
+        /*int fatResult = 3;
+
+        for (int i = 0; i >1; i--) {
+            if(i ==0){
+                fatResult = 1;
+                break;
+            }else{
+
+                fatResult = i * fatResult;
+            }
+           }
+         System.out.println(fatResult);  
 }
+} */
